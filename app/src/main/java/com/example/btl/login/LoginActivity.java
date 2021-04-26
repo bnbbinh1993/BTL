28e02087d7e86ce329e9f7692f76e8ba318af050
Vao day <bnbbinh@gmail.com>
2021-04-26 14:46:03 +0700

    cmt

--- app/src/main/java/com/example/btl/login/LoginActivity.java
+++ app/src/main/java/com/example/btl/login/LoginActivity.java
@@ -1,16 +1,106 @@
 package com.example.btl.login;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.btl.R;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+    private MaterialButton btnGG;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth auth;
+    private static final int RC_SIGN_IN = 100;
+    private FacebookSdk facebookSdk;
+    private AppEventsLogger appEventsLogger;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+
+        auth = FirebaseAuth.getInstance();
+
+
+        btnGG = findViewById(R.id.btnGG);
+        btnGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, RC_SIGN_IN);
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount inAccount = accountTask.getResult(ApiException.class);
+                if (inAccount != null) {
+                    if (inAccount.getPhotoUrl() != null) {
+                        Toast.makeText(this, "Đã lấy đc ảnh", Toast.LENGTH_SHORT).show();
+                    }
+                    firebaseAuth(inAccount);
+                }
+
+
+            } catch (Exception e) {
+                Toast.makeText(this, "" + e, Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    private void firebaseAuth(GoogleSignInAccount inAccount) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(inAccount.getIdToken(), null);
+        auth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                FirebaseUser user = auth.getCurrentUser();
+                Toast.makeText(LoginActivity.this, "Đã login thành công!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+
 }
